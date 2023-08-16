9bf909c86a3fe5d037362ec3cad87c5ba6aa9ef6
Viet Le Nguyen Ngoc <viet.lenguyenngoc@emea.nttdata.com>
2023-08-16 18:37:46 +0700

    Initial commit

---
+++ src/test/java/com/vietlnn/aws/springbootlambda/SpringbootLambdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vietlnn.aws.springbootlambda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLambdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
